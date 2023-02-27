22ec6fda9586f9de7a80b44f21ac7ccb552d7952
chtsangak <chtsangak@connect.ust.hk>
2023-02-27 17:47:32 +0800

    first commit

---
+++ src/solution.java
@@ -0,0 +1,8 @@
+public class solution{
+
+    public static void main(String[] s)
+    {
+        System.out.println("Hello world");
+        System.out.println("This is my first Java program");
+    }
+}
